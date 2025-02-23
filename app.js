d2f41b1b1ae57b0c869aeb665556611d4f51f358
Ben Xiao <benssiao@gmail.com>
2025-02-23 10:54:54 -0800

    ok

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 //connection middleware
 app.use("/api/blogs", blogsRouter);
-
 app.use("/api/users", usersRouter);
 app.use('/api/login', loginRouter)
-
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./connections/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 // error and catch all middleware
 app.use(middleware.unknownEndpoint);
